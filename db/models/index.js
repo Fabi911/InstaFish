dd0358a829541832e5c038bde2d620ca41c5157d
Fabian Döz <fabian@doez.info>
2024-05-06 10:02:59 +0200

    update

---
+++ db/models/index.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const catchSchema = new Schema({
+  date: { type: String, required: true },
+  location: { type: String, required: true },
+  species: { type: String, required: true },
+  size: { type: Number },
+  weight: { type: Number, required: true },
+  image: { type: String },
+  favorite: { type: Boolean },
+  notes: { type: [String] },
+});
+
+const Catch = mongoose.models.Catch || mongoose.model("Catch", catchSchema);
+
+export default Catch;
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,11 @@
+import useSWR from "swr";
+
 export default function Home() {
-  return <></>;
+  const { data, isLoading, error } = useSWR("/api/catches");
+  if (error) return <div>failed to load</div>;
+  if (isLoading) return <div>loading...</div>;
+
+  console.log(data);
+
+  return <>Hello</>;
 }
