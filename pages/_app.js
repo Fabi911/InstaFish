9d83741369771537c3cd69f7c73dfc4a2440ef64
Fabian Döz <fabian@doez.info>
2024-08-05 16:26:52 +0200

    change bg

--- pages/_app.js
+++ pages/_app.js
@@ -1,93 +1,94 @@
 import GlobalStyle from "../styles";
-import { SWRConfig } from "swr";
-import { ToastContainer, toast } from "react-toastify";
+import {SWRConfig} from "swr";
+import {ToastContainer, toast} from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { useRouter } from "next/router";
+import {useRouter} from "next/router";
 import Layout from "@/components/Layout/Layout";
-import { SessionProvider } from "next-auth/react";
+import {SessionProvider} from "next-auth/react";
 
-export default function App({ Component, pageProps }) {
-  const router = useRouter();
-  async function handleAddCatch(catchItem) {
-    const response = await toast.promise(
-      fetch("/api/catches", {
-        method: "POST",
-        body: JSON.stringify(catchItem),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }),
-      {
-        pending: "adding is pending",
-        success: "Catch added! 👌",
-        error: "adding rejected 🤯",
-      }
-    );
+export default function App({Component, pageProps}) {
+    const router = useRouter();
 
-    if (response.ok) {
-      await response.json();
-      router.push("/CatchOverviewPage");
-    } else {
-      console.error(`Error: ${response.status}`);
+    async function handleAddCatch(catchItem) {
+        const response = await toast.promise(
+            fetch("/api/catches", {
+                method: "POST",
+                body: JSON.stringify(catchItem),
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            }),
+            {
+                pending: "adding is pending",
+                success: "Catch added! 👌",
+                error: "adding rejected 🤯",
+            }
+        );
+
+        if (response.ok) {
+            await response.json();
+            router.push("/CatchOverviewPage");
+        } else {
+            console.error(`Error: ${response.status}`);
+        }
     }
-  }
 
-  async function handleDeleteCatch(id, mutate) {
-    const response = await toast.promise(
-      fetch(`/api/catches/${id}`, {
-        method: "DELETE",
-      }),
-      {
-        pending: "deleting is pending",
-        success: "Catch deleted! 👌",
-        error: "deleting rejected 🤯",
-      }
-    );
+    async function handleDeleteCatch(id, mutate) {
+        const response = await toast.promise(
+            fetch(`/api/catches/${id}`, {
+                method: "DELETE",
+            }),
+            {
+                pending: "deleting is pending",
+                success: "Catch deleted! 👌",
+                error: "deleting rejected 🤯",
+            }
+        );
 
-    if (response.ok) {
-      router.push("/CatchOverviewPage");
-      mutate();
-    } else {
-      console.error(response.status);
+        if (response.ok) {
+            router.push("/CatchOverviewPage");
+            mutate();
+        } else {
+            console.error(response.status);
+        }
     }
-  }
 
-  return (
-    <SessionProvider session={pageProps.session}>
-      <Layout>
-        <div className="background"></div>
-        <GlobalStyle />
-        <SWRConfig
-          value={{
-            fetcher: async (...args) => {
-              const response = await fetch(...args);
-              if (!response.ok) {
-                throw new Error(`Request with ${JSON.stringify(args)} failed.`);
-              }
-              return await response.json();
-            },
-          }}
-        >
-          <ToastContainer
-            position="top-center"
-            autoClose={5000}
-            hideProgressBar={false}
-            newestOnTop
-            closeOnClick
-            rtl={false}
-            pauseOnFocusLoss
-            draggable
-            pauseOnHover
-            theme="dark"
-          />
+    return (
+        <SessionProvider session={pageProps.session}>
+            <Layout>
+                {/*<div className="background"></div>*/}
+                <GlobalStyle/>
+                <SWRConfig
+                    value={{
+                        fetcher: async (...args) => {
+                            const response = await fetch(...args);
+                            if (!response.ok) {
+                                throw new Error(`Request with ${JSON.stringify(args)} failed.`);
+                            }
+                            return await response.json();
+                        },
+                    }}
+                >
+                    <ToastContainer
+                        position="top-center"
+                        autoClose={5000}
+                        hideProgressBar={false}
+                        newestOnTop
+                        closeOnClick
+                        rtl={false}
+                        pauseOnFocusLoss
+                        draggable
+                        pauseOnHover
+                        theme="dark"
+                    />
 
-          <Component
-            {...pageProps}
-            onSubmit={handleAddCatch}
-            handleDeleteCatch={handleDeleteCatch}
-          />
-        </SWRConfig>
-      </Layout>
-    </SessionProvider>
-  );
+                    <Component
+                        {...pageProps}
+                        onSubmit={handleAddCatch}
+                        handleDeleteCatch={handleDeleteCatch}
+                    />
+                </SWRConfig>
+            </Layout>
+        </SessionProvider>
+    );
 }
