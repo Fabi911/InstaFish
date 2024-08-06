8fd536b18d27440ce30f901675d8d3d37bf65082
Fabian Döz <fabian@doez.info>
2024-08-06 14:05:22 +0200

    add dropdown on species

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,8 +1,8 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import CredentialsProvider from "next-auth/providers/credentials";
-import { MongoDBAdapter } from "@auth/mongodb-adapter";
-import { MongoClient } from "mongodb";
+import {MongoDBAdapter} from "@auth/mongodb-adapter";
+import {MongoClient} from "mongodb";
 import bcrypt from "bcryptjs";
 
 const client = new MongoClient(process.env.MONGODB_URI);
@@ -10,79 +10,89 @@ const client = new MongoClient(process.env.MONGODB_URI);
 const clientPromise = client.connect();
 
 export const authOptions = {
-  providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-    }),
-    CredentialsProvider({
-      name: "credentials",
-      credentials: {
-        username: { label: "Username", type: "text", placeholder: "username" },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials) {
-        if (
-          credentials.username === process.env.USERNAME_HEINI &&
-          credentials.password === process.env.PASSWORD_HEINI
-        ) {
-          return {
-            name: "Heinrich",
-            email: "user@test.de",
-          };
-        } else {
-          return null;
-        }
-      },
-    }),
-    //   CredentialsProvider({
-    //     name: "Credentials",
-    //     credentials: {
-    //       email: {
-    //         label: "Email",
-    //         type: "email",
-    //         placeholder: "email@example.com",
-    //       },
-    //       password: { label: "Password", type: "password" },
-    //     },
-    //     async authorize(credentials, req) {
-    //       const db = (await clientPromise).db();
-    //       const user = await db
-    //         .collection("users")
-    //         .findOne({ email: credentials.email });
+    providers: [
+        GithubProvider({
+            clientId: process.env.GITHUB_ID,
+            clientSecret: process.env.GITHUB_SECRET,
+        }),
+        CredentialsProvider({
+            name: "credentials",
+            credentials: {
+                username: {label: "Username", type: "text", placeholder: "username"},
+                password: {label: "Password", type: "password"},
+            },
+            async authorize(credentials) {
+                if (
+                    credentials.username === process.env.USERNAME_HEINI &&
+                    credentials.password === process.env.PASSWORD_HEINI
+                ) {
+                    return {
+                        name: "Heinrich",
+                        email: "user@test.de",
+                    };
+                }
+                if (
+                    credentials.username === "volkmar" &&
+                    credentials.password === "test1234"
+                ) {
+                    return {
+                        name: "Volkmar",
+                        email: "user2@test.de",
+                    };
+                } else {
+                    return null;
+                }
+            },
+        }),
 
-    //       if (user && bcrypt.compareSync(credentials.password, user.password)) {
-    //         return {
-    //           name: credentials.name,
-    //           email: credentials.email,
-    //         };
-    //       } else {
-    //         return null;
-    //       }
-    //     },
-    //   }),
-  ],
-  // adapter: MongoDBAdapter(clientPromise),
-  // secret: process.env.SECRET,
-  // session: {
-  //   jwt: true,
-  // },
-  // jwt: {
-  //   secret: process.env.SECRET,
-  //   encryption: true,
-  // },
-  // callbacks: {
-  //   async session(session, user) {
-  //     session.user.id = user.id;
-  //     return session;
-  //   },
-  //   async jwt(token, user) {
-  //     if (user) {
-  //       token.id = user.id;
-  //     }
-  //     return token;
-  //   },
-  // },
+        //   CredentialsProvider({
+        //     name: "Credentials",
+        //     credentials: {
+        //       email: {
+        //         label: "Email",
+        //         type: "email",
+        //         placeholder: "email@example.com",
+        //       },
+        //       password: { label: "Password", type: "password" },
+        //     },
+        //     async authorize(credentials, req) {
+        //       const db = (await clientPromise).db();
+        //       const user = await db
+        //         .collection("users")
+        //         .findOne({ email: credentials.email });
+
+        //       if (user && bcrypt.compareSync(credentials.password, user.password)) {
+        //         return {
+        //           name: credentials.name,
+        //           email: credentials.email,
+        //         };
+        //       } else {
+        //         return null;
+        //       }
+        //     },
+        //   }),
+    ],
+    // adapter: MongoDBAdapter(clientPromise),
+    // secret: process.env.SECRET,
+    // session: {
+    //   jwt: true,
+    // },
+    // jwt: {
+    //   secret: process.env.SECRET,
+    //   encryption: true,
+    // },
+    // callbacks: {
+    //   async session(session, user) {
+    //     session.user.id = user.id;
+    //     return session;
+    //   },
+    //   async jwt(token, user) {
+    //     if (user) {
+    //       token.id = user.id;
+    //     }
+    //     return token;
+    //   },
+    // },
 };
 
 export default NextAuth(authOptions);
