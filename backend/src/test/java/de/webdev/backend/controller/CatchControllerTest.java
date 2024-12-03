e5c61b3d638118487f1f8f76bcdf1e962fc3df67
Fabian Döz <fabian@doez.info>
2024-12-03 23:34:19 +0100

    - add docker files - remove nextAuth

--- backend/src/test/java/de/webdev/backend/controller/CatchControllerTest.java
+++ backend/src/test/java/de/webdev/backend/controller/CatchControllerTest.java
@@ -1,6 +1,5 @@
 package de.webdev.backend.controller;
 
-import de.webdev.backend.model.Catch;
 import de.webdev.backend.repository.CatchRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,88 +10,69 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 class CatchControllerTest {
 
-    @Autowired
-    private MockMvc mockMvc;
+	@Autowired
+	private MockMvc mockMvc;
 
-    @Autowired
-    private CatchRepository catchRepository;
+	@Autowired
+	private CatchRepository catchRepository;
 
-    @Test
-    void getAllCatches() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/api/catch"))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.content().json("[]"));
-    }
-
-    @DirtiesContext
-    @Test
-    void getAllCatches_with_a_list() throws Exception {
-        Catch catch1 = new Catch("1", "2021-08-01", "Hamburg", "Zander", 50.0, 2.0, "image.jpg", "true", List.of("note1", "note2"), "author", "method", "bait");
-        Catch catch2 = new Catch("2", "2021-08-02", "Berlin", "Hecht", 60.0, 3.0, "image.jpg", "false", List.of("note1", "note2"), "author", "method", "bait");
-        Catch catch3 = new Catch("3", "2021-08-03", "München", "Barsch", 40.0, 1.0, "image.jpg", "true", List.of("note1", "note2"), "author", "method", "bait");
-
-        catchRepository.save(catch1);
-        catchRepository.save(catch2);
-        catchRepository.save(catch3);
-
-        mockMvc.perform(MockMvcRequestBuilders.get("/api/catch"))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.content().json("[{\"id\":\"1\",\"date\":\"2021-08-01\",\"location\":\"Hamburg\",\"species\":\"Zander\",\"size\":50.0,\"weight\":2.0,\"image\":\"image.jpg\",\"favorite\":\"true\",\"notes\":[\"note1\",\"note2\"],\"author\":\"author\",\"method\":\"method\",\"bait\":\"bait\"},{\"id\":\"2\",\"date\":\"2021-08-02\",\"location\":\"Berlin\",\"species\":\"Hecht\",\"size\":60.0,\"weight\":3.0,\"image\":\"image.jpg\",\"favorite\":\"false\",\"notes\":[\"note1\",\"note2\"],\"author\":\"author\",\"method\":\"method\",\"bait\":\"bait\"},{\"id\":\"3\",\"date\":\"2021-08-03\",\"location\":\"München\",\"species\":\"Barsch\",\"size\":40.0,\"weight\":1.0,\"image\":\"image.jpg\",\"favorite\":\"true\",\"notes\":[\"note1\",\"note2\"],\"author\":\"author\",\"method\":\"method\",\"bait\":\"bait\"}]"));
-    }
+	@Test
+	void getAllCatches() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/api/catch"))
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.content().json("[]"));
+	}
 
 
-    @DirtiesContext
-    @Test
-    void saveCatch_with_a_catch() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.post("/api/catch")
-                        .contentType("application/json")
-                        .content(
-                                """
-                                                {
-                                                        "id": "66b265cd503c9158469b52522",
-                                                        "date": "2024-08-06",
-                                                          "location": "Leineck",
-                                                        "species": "Karpfen",
-                                                        "size": 50.0,
-                                                        "weight": 3.0,
-                                                         "image": "",
-                                                "favorite": "false",
-                                                "notes": [
-                                                            ""
-                                                        ],
-                                                        "author": "user2@test.de",
-                                                        "methode":"",
-                                                        "bait": ""
-                                                    }
-                                        """
-                        ))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.content().json("""                    
-                            {
-                                                                        "id": "66b265cd503c9158469b52522",
-                                                                        "date": "2024-08-06",
-                                                                        "location": "Leineck",
-                                                                        "species": "Karpfen",
-                                                                        "size": 50.0,
-                                                                        "weight": 3.0,
-                                                                        "image": "",
-                                                                        "favorite": "false",
-                                                                        "notes": [
-                                                                            ""
-                                                                        ],
-                                                                        "author": "user2@test.de",
-                                                                        "methode":"",
-                                                                        "bait": ""
-                          }
-                        """
-                ));
-    }
+	@DirtiesContext
+	@Test
+	void saveCatch_with_a_catch() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.post("/api/catch")
+						.contentType("application/json")
+						.content(
+								"""
+										        {
+										                "id": "66b265cd503c9158469b52522",
+										                "date": "2024-08-06",
+										                  "location": "Leineck",
+										                "species": "Karpfen",
+										                "size": 50.0,
+										                "weight": 3.0,
+										                 "image": "",
+										        "favorite": "false",
+										        "notes": [
+										                    ""
+										                ],
+										                "author": "user2@test.de",
+										                "methode":"",
+										                "bait": ""
+										            }
+										"""
+						))
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.content().json("""                    
+						    {
+						                                                "id": "66b265cd503c9158469b52522",
+						                                                "date": "2024-08-06",
+						                                                "location": "Leineck",
+						                                                "species": "Karpfen",
+						                                                "size": 50.0,
+						                                                "weight": 3.0,
+						                                                "image": "",
+						                                                "favorite": "false",
+						                                                "notes": [
+						                                                    ""
+						                                                ],
+						                                                "author": "user2@test.de",
+						                                                "methode":"",
+						                                                "bait": ""
+						  }
+						"""
+				));
+	}
 }
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,64 +1,49 @@
 import Router, {useRouter} from "next/router";
 import styled from "styled-components";
-import {useSession} from "next-auth/react";
 import CatchCard from "@/components/catchList/CatchCard";
-import useSWR from "swr";
 import {useEffect, useState} from "react";
 import axios from "axios";
 
 export default function Home({handleDeleteCatch}) {
-    const {data: session} = useSession();
-    const router = useRouter()
-    const [catchDataHome, setCatchDataHome] = useState([])
-
-    const fetchDataHome = () => {
-        axios.get('/api/catch')
-            .then((response) => {
-                setCatchDataHome(response.data)
-            })
-            .catch((error) => {
-                console.log(error)
-            })
-    }
-
-    useEffect(() => {
-        fetchDataHome()
-    }, [])
-
-    /*if (!session) {
-        return <p>Bitte anmelden</p>*/
-    /*} else {*/
-
-
-    const lastCatch = catchDataHome[catchDataHome.length - 1];
-    if (!lastCatch) {
-        return null
-    }
-
-    return (
-        <ContainerMain>
-            {/* <H2>Willkommen {session.user.name}</H2>*/}
-            <ContentBox>
-                <ButtonNewCatch onClick={() => Router.push("/add")}>
-                    neuen Fang eintragen
-                </ButtonNewCatch>
-            </ContentBox>
-            <LastCatch>
-                <H3LastCatch>Dein letzter Fang</H3LastCatch>
-                <CatchCard data={lastCatch} onclickDeleteCatch={handleDeleteCatch}/>
-            </LastCatch>
-        </ContainerMain>
-    )
-    /*}*/
-
+	const router = useRouter()
+	const [catchDataHome, setCatchDataHome] = useState([])
+	const fetchDataHome = () => {
+		axios.get('/api/catch')
+			.then((response) => {
+				setCatchDataHome(response.data)
+			})
+			.catch((error) => {
+				console.log(error)
+			})
+	}
+	useEffect(() => {
+		fetchDataHome()
+	}, [])
+	const lastCatch = catchDataHome[catchDataHome.length - 1];
+	if (!lastCatch) {
+		return null
+	}
+	return (
+		<ContainerMain>
+			{/* <H2>Willkommen {session.user.name}</H2>*/}
+			<ContentBox>
+				<ButtonNewCatch onClick={() => Router.push("/add")}>
+					neuen Fang eintragen
+				</ButtonNewCatch>
+			</ContentBox>
+			<LastCatch>
+				<H3LastCatch>Dein letzter Fang</H3LastCatch>
+				<CatchCard data={lastCatch} onclickDeleteCatch={handleDeleteCatch}/>
+			</LastCatch>
+		</ContainerMain>
+	)
+	/*}*/
 }
-
 const ContainerMain = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
 `
-
 const ContentBox = styled.div`
     display: flex;
     flex-direction: column;
@@ -66,7 +51,6 @@ const ContentBox = styled.div`
     gap: 10px;
     margin: 0.5rem 0 1rem 0;
 `
-
 const LastCatch = styled.div`
     display: flex;
     flex-direction: column;
@@ -77,12 +61,9 @@ const LastCatch = styled.div`
 const H2 = styled.h2`
     font-size: 1.4rem;
 `
-
-
 const H3LastCatch = styled.h3`
     margin-bottom: 0.2rem;
 `
-
 const ButtonNewCatch = styled.button`
             font-size: 1.2rem;
             padding: 0.5rem 1rem;
@@ -94,5 +75,5 @@ const ButtonNewCatch = styled.button`
             &:active {
                 box-shadow: inset var(--box-shadow-default);
             }
-    `
+	`
 ;
